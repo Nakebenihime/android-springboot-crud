4993986bb937b0ea77e1c2094b86d098f14725dc
mario.dacosta <mario-dacosta@hotmail.fr>
2020-12-13 16:01:27 +0100

    - MangaModel added with @Entity and @GeneratedValue annotations

--- android-springboot-backend/src/main/java/org/androidspringbootbackend/model/MangaModel.java
+++ android-springboot-backend/src/main/java/org/androidspringbootbackend/model/MangaModel.java
@@ -1,4 +1,26 @@
 package org.androidspringbootbackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class MangaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean status;
 }
