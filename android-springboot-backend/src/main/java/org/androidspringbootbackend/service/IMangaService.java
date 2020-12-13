2f7436d7c841b055e39fdded1ce5a6fdb51774fa
mario.dacosta <mario-dacosta@hotmail.fr>
2020-12-13 22:44:13 +0100

    - Interface IMangaService created

--- android-springboot-backend/src/main/java/org/androidspringbootbackend/service/IMangaService.java
+++ android-springboot-backend/src/main/java/org/androidspringbootbackend/service/IMangaService.java
@@ -1,4 +1,13 @@
 package org.androidspringbootbackend.service;
 
-public interface IMangaService {
+import java.util.List;
+import java.util.Optional;
+
+public interface IMangaService<Manga> {
+
+    Manga save(Manga t);
+    Optional<Manga> findById(long id);
+    void delete(Manga Manga);
+    List<Manga> findAll();
+    void deleteById(long id);
 }
