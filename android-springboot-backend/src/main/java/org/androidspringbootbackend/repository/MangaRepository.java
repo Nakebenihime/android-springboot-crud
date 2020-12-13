50fdaa738867d2e68ef317b69889c3b8d085e98c
mario.dacosta <mario-dacosta@hotmail.fr>
2020-12-13 22:39:01 +0100

    - MangaRepository JpaRepository interface created

--- android-springboot-backend/src/main/java/org/androidspringbootbackend/repository/MangaRepository.java
+++ android-springboot-backend/src/main/java/org/androidspringbootbackend/repository/MangaRepository.java
@@ -1,7 +1,9 @@
 package org.androidspringbootbackend.repository;
 
+import org.androidspringbootbackend.model.Manga;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MangaRepository {
+public interface MangaRepository extends JpaRepository<Manga, Long> {
 }
