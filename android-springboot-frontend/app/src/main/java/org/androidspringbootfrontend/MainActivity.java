f504dbcc519177bc62b24e8a3b4d891f7dd5268d
mario.dacosta <mario-dacosta@hotmail.fr>
2020-12-23 22:18:02 +0100

    - project structure + gradle

---
+++ android-springboot-frontend/app/src/main/java/org/androidspringbootfrontend/MainActivity.java
@@ -0,0 +1,74 @@
+package org.androidspringbootfrontend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    CustomAdapter customAdapter;
+    ListView view;
+    final MangaService service = ServiceGenerator.createService(MangaService.class);
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        view = (ListView) findViewById((R.id.view_mangas));
+        view.setOnItemClickListener((parent, view, position, id) -> {
+            final Manga manga = (Manga) parent.getItemAtPosition(position);
+            Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+            intent.putExtra("id", manga.getId());
+            startActivity(intent);
+        });
+        this.findAll();
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    public void findAll() {
+        Call<List<Manga>> call = service.findAll();
+        call.enqueue(new Callback<List<Manga>>() {
+            @Override
+            public void onResponse(Call<List<Manga>> call, Response<List<Manga>> response) {
+                if (response.isSuccessful()){
+                    List<Manga> mangas = response.body();
+                    assert mangas != null;
+                    customAdapter = new CustomAdapter(getApplicationContext(), mangas);
+                    view.setAdapter(customAdapter);
+                } else {
+                    System.out.println(response.body());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Manga>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.findAll();
+    }
+}
---
+++ android-springboot-frontend/app/src/main/java/org/androidspringbootfrontend/SecondaryActivity.java
@@ -0,0 +1,136 @@
+package org.androidspringbootfrontend;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SecondaryActivity extends Activity {
+    Manga manga;
+    Button btnUpdate;
+    Button btnDelete;
+    EditText textTitle;
+    EditText textAuthor;
+    EditText textYear;
+    EditText textStatus;
+    ImageView imageView;
+
+    final MangaService service = ServiceGenerator.createService(MangaService.class);
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manga_details);
+
+        btnUpdate = findViewById(R.id.btn_update);
+        btnDelete = findViewById(R.id.btn_delete);
+        textTitle = findViewById(R.id.title);
+        textAuthor = findViewById(R.id.author);
+        textYear = findViewById(R.id.year);
+        textStatus = findViewById(R.id.status);
+        imageView = findViewById(R.id.image_view_detail);
+
+        final Intent intent = getIntent();
+        final long id = intent.getLongExtra("id", 0);
+
+        btnUpdate.setOnClickListener(v -> {
+            updateById(id);
+            finish();
+        });
+
+        btnDelete.setOnClickListener(v -> {
+            deleteById(id);
+            finish();
+        });
+        findById(id);
+    }
+
+    public void setFindById(Manga manga) {
+
+        final String title = manga.getTitle();
+        textTitle.setText(title);
+
+        final String author = manga.getAuthor();
+        textAuthor.setText(author);
+
+        final String year = Integer.toString(manga.getYear());
+        textYear.setText(year);
+
+        final String status = Boolean.toString(manga.isStatus());
+        textStatus.setText(status);
+
+        final String URL = manga.getURL();
+        Picasso.get().load(URL).centerCrop().fit().into(imageView);
+    }
+        public void findById(long id) {
+            Call<Manga> call = service.findById(id);
+            call.enqueue(new Callback<Manga>() {
+                @Override
+                public void onResponse(Call<Manga> call, Response<Manga> response) {
+                    if (response.isSuccessful()) {
+                        manga = response.body();
+                        assert manga != null;
+                        setFindById(manga);
+                        Toast.makeText(SecondaryActivity.this, "FindById operation succeed!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SecondaryActivity.this, "FindById operation failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Manga> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+        public void deleteById(long id) {
+            Call<Manga> call = service.delete(id);
+            call.enqueue(new Callback<Manga>() {
+                @Override
+                public void onResponse(Call<Manga> call, Response<Manga> response) {
+                    if (response.isSuccessful()) {
+                        Toast.makeText(SecondaryActivity.this, "DeleteById operation succeed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Manga> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+        public void updateById(long id) {
+            Manga modified = new Manga(id, textTitle.getText().toString(), textAuthor.getText().toString(), Integer.parseInt(textYear.getText().toString()), Boolean.parseBoolean(textStatus.getText().toString()), manga.getURL());
+            Call<Manga> call = service.update(modified);
+            call.enqueue(new Callback<Manga>() {
+                @Override
+                public void onResponse(Call<Manga> call, Response<Manga> response) {
+                    if (response.isSuccessful()) {
+                        Toast.makeText(SecondaryActivity.this, "UpdateById operation succeed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Manga> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+    }
