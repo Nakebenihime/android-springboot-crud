679340b6e941465911b34609933e5978e161df02
mario.dacosta <mario-dacosta@hotmail.fr>
2020-12-23 22:14:58 +0100

    - activity_main.xml + activity_manga_details.xml + row.xml added - CustomAdapter added

---
+++ android-springboot-frontend/app/src/main/java/org/androidspringbootfrontend/CustomAdapter.java
@@ -0,0 +1,47 @@
+package org.androidspringbootfrontend;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CustomAdapter  extends ArrayAdapter<Manga> {
+
+    private final Context context;
+    private final List<Manga> mangas;
+
+
+    public CustomAdapter(@NonNull Context context, @NonNull List<Manga> mangas) {
+        super(context,0, mangas);
+        this.context = context;
+        this.mangas = mangas;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @SuppressLint("ViewHolder") View rowView = inflater.inflate(R.layout.row, parent, false);
+        Manga manga = mangas.get(position);
+
+        TextView name = rowView.findViewById(R.id.row_view);
+        name.setText(manga.getTitle());
+
+        ImageView imageView = rowView.findViewById(R.id.mini);
+        Picasso.get().load(manga.getURL()).centerCrop().fit().into(imageView);
+
+        return rowView;
+    }
+}
