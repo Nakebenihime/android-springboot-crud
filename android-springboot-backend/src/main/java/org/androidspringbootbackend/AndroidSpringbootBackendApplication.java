25b06d5252d280c6bd5d11731d52918547d19433
mario.dacosta <mario-dacosta@hotmail.fr>
2021-01-02 17:47:15 +0100

    - $PORT added to Dockerfile

--- android-springboot-backend/src/main/java/org/androidspringbootbackend/AndroidSpringbootBackendApplication.java
+++ android-springboot-backend/src/main/java/org/androidspringbootbackend/AndroidSpringbootBackendApplication.java
@@ -1,8 +1,13 @@
 package org.androidspringbootbackend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class AndroidSpringbootBackendApplication {
 
@@ -10,4 +15,13 @@ public class AndroidSpringbootBackendApplication {
         SpringApplication.run(AndroidSpringbootBackendApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            log.info("PORT {}", environment.getProperty("server.port"));
+            log.info("URL {}", environment.getProperty("spring.datasource.url"));
+            log.info("PASSWORD {}",environment.getProperty("spring.datasource.password"));
+            log.info("USERNAME {}",environment.getProperty("spring.datasource.username"));
+        };
+    }
 }
